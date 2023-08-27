10f29d6840c9151ed757b4b5b1e624f2c4cd72b5
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-27 16:51:16 -0500

    added canvas to lightning

--- game.js
+++ game.js
@@ -8,8 +8,8 @@ import {
     createWraithObstacle, createZombieDragonObstacle, createThundercloudObstacle, createFireballObstacle
 } from './obstacles.js';
 
-let obstacleSpawnTime = 4000, topObstacle = false, obstacleY, spawnRate = 5, spawnTimer = 0, framesPerFlap = 100, 
-    gameLoopCounter = 0, gameStarted = false, jump = 8, isFlapping = false, dragonFlapSpeed = 3;
+let obstacleSpawnTime = 4000, topObstacle = false, obstacleY, spawnRate = 5, spawnTimer = 0, framesPerFlap = 100, gameLoopCounter = 0, gameStarted = false, 
+    jump = 8, isFlapping = false, dragonFlapSpeed = 3, let lightningStrike = createLightningStrikeObstacle(startX, startY, dragon.x, dragon.y, canvas, context);
 
 // To prevent multiple jumps
 let jumpLock = false;
