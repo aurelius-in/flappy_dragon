681ff0c3c1b3b009a7e81d0fba8f77dc554f7f53
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-27 17:15:34 -0500

    fixed lighting effect

--- game.js
+++ game.js
@@ -9,8 +9,7 @@ import {
 } from './obstacles.js';
 
 let obstacleSpawnTime = 4000, topObstacle = false, obstacleY, spawnRate = 5, spawnTimer = 0, framesPerFlap = 100, gameLoopCounter = 0, gameStarted = false, jump = 8, isFlapping = false, dragonFlapSpeed = 3, 
-    lightningStartX = canvas.width, lightningStartY = 0, lightningStrike = createLightningStrikeObstacle(lightningStartX, lightningStartY, dragon.x, dragon.y, canvas, context);
-
+    lightningStartX = canvas.width / 2, lightningStartY = 0, lightningStrike = createLightningStrikeObstacle(lightningStartX, lightningStartY, dragon.x + 20, dragon.y - 20, canvas, context);
 
 // To prevent multiple jumps
 let jumpLock = false;
