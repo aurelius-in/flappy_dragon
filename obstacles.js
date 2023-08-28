96d3d44f2022fc068bd40109fc3344015c7d3eb7
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-28 03:43:34 -0500

    Update obstacles.js

--- obstacles.js
+++ obstacles.js
@@ -51,52 +51,35 @@ function createArrowObstacle(x, y) {
 }
 
 // Lightning Strikes
-function createLightningStrikeObstacle(x, y, dragonX, dragonY, canvas, context) {
-    let lastStrikeTime = Date.now();  // Initialize to current time
-    let fadeAlpha = 0;  // Alpha value for the fade effect
-    let flashCounter = 0;  // Counter for screen flashes
+function createLightningStrikeObstacle(dragonX, dragonY, canvas, context) {
+    let lastStrikeTime = Date.now();
+    let fadeAlpha = 0;
+    let flashCounter = 0;
 
     return {
-        x: canvas.width / 2,  // Start from the center of the canvas
-        y: 0,  // Start from the top edge of the screen
+        x: canvas.width / 2,
+        y: 0,
         width: 10,
         height: 200,
-        targetX: dragonX + 20,
-        targetY: dragonY + 20,
-        zigzagCounter: 0,
         update: function() {
             let currentTime = Date.now();
 
-            // Targeted strike towards the dragon's position
-            let dx = this.targetX - this.x;
-            let dy = this.targetY - this.y;
-            let distance = Math.sqrt(dx * dx + dy * dy);
-
-            // Normalize the distance and move towards the dragon
-            this.x += (dx / distance) * 2;
-            this.y += (dy / distance) * 2;
-
-            // Add zigzag movement
-            this.x += Math.sin(this.zigzagCounter) * 10;
-            this.zigzagCounter += 0.1;
-
-            // Trigger the fade effect and screen flash
-            if (currentTime - lastStrikeTime > 5000) {  // 5 seconds
+            // Update periodically based on the game's time
+            if (currentTime - lastStrikeTime > 5000) {
                 lastStrikeTime = currentTime;
-                fadeAlpha = 0.7;  // Set a high alpha value for a bright flash
-                flashCounter = 2;  // Reset flash counter
+                flashCounter = 2;
             }
 
             if (flashCounter > 0) {
-                fadeAlpha = Math.max(0, fadeAlpha - 0.02);  // Gradually reduce the alpha value
+                fadeAlpha = Math.max(0, fadeAlpha - 0.02);
                 if (fadeAlpha === 0) {
-                    flashCounter--;  // Decrease flash counter
-                    fadeAlpha = 0.7;  // Reset alpha for next flash
+                    flashCounter--;
+                    fadeAlpha = 0.7;
                 }
             }
         },
         draw: function() {
-            context.fillStyle = `rgba(255, 255, 0, ${fadeAlpha})`;  // Use fadeAlpha for the alpha channel
+            context.fillStyle = `rgba(255, 255, 0, ${fadeAlpha})`;
             context.fillRect(this.x, this.y, this.width, this.height);
         }
     };
