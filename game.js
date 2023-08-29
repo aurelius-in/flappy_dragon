dbe7dc38aef921f7ec593bce4cd6fe1f6e3cb2f7
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-28 19:09:11 -0500

    Update game.js

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ import {
 } from './init.js';
 import { draw } from './render.js';
 import {
-    createArrowObstacle, createLightningStrikeObstacle, createBatSwarmObstacle, createTornadoObstacle,
+    obstacle, createArrowObstacle, createLightningStrikeObstacle, createBatSwarmObstacle, createTornadoObstacle,
     createWraithObstacle, createZombieDragonObstacle, createThundercloudObstacle, createFireballObstacle
 } from './obstacles.js';
 
