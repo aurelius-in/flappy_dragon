df11c1584c7629605127b9240cf8e0f6482a9c00
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-08-30 04:14:50 -0500

    added lev2 bga

--- render.js
+++ render.js
@@ -32,13 +32,16 @@ function screenFlicker() {
   export function draw() {
     // Draw the furthest back background (bgbg)
     context.drawImage(bgbgImage, backgrounds.bgbgX, 0, bg.width, backgrounds.height);
-
+    context.drawImage(backgrounds.bgbgImage, backgrounds.bgbgX, 0, bg.width, backgrounds.height);
+    
     // Draw the middle background (bg)
     context.drawImage(bgImage, backgrounds.bgX, 0, bg.width, backgrounds.height);
+    context.drawImage(backgrounds.bgImage, backgrounds.bgX, 0, bg.width, backgrounds.height);
 
     // Draw the closest background (fg)
     context.drawImage(fgImage, backgrounds.fgX, 0, bg.width, backgrounds.height);
-
+    context.drawImage(backgrounds.fgImage, backgrounds.fgX, 0, bg.width, backgrounds.height);
+    
     // Draw perch
     context.drawImage(perchImage, perch.x, perchY, perchWidth, perchHeight);
 
@@ -81,7 +84,7 @@ if (tapToFly.alpha > 0) {
     context.fillText('TAP TO FLY!', canvas.width / 2, canvas.height / 2);
 }
 
-   // Draw black fade overlay
+   // Draw black fade1 overlay
     context.fillStyle = `rgba(0, 0, 0, ${screenFade.alpha})`;
     context.fillRect(0, 0, canvas.width, canvas.height);
 
